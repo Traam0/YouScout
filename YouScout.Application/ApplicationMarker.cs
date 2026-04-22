2a5254ab15de3bd6335ce17b811ec973c4a97577
Traam0 <younesmrx@outlook.com>
2026-04-22 18:15:14 +0100

    [feat]: added ApplicationLayer project to the solution

---
+++ YouScout.Application/ApplicationMarker.cs
@@ -0,0 +1,11 @@
+﻿namespace YouScout.Application;
+
+/// <summary>
+/// Marker type used to reference the <c>YouScout.Application</c> assembly.
+/// </summary>
+/// <remarks>
+/// This class is typically used for reflection-based registration, such as scanning for services,
+/// validators, mappings, or configuration defined in this assembly.
+/// DO NOT REMOVE !!
+/// </remarks>
+public sealed class ApplicationMarker;
