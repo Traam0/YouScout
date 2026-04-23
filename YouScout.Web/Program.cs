fea471c690b028a6381dc5011fed69d33aa5e18d
Traam0 <younesmrx@outlook.com>
2026-04-23 13:47:35 +0100

    [add.Web]: initializing and running inital migration.

--- YouScout.Web/Program.cs
+++ YouScout.Web/Program.cs
@@ -1,3 +1,7 @@
+using YouScout.Application;
+using YouScout.Infrastructure;
+using YouScout.Infrastructure.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .RegisterApplicationLayer()
+    .RegisterInfrastructureLayer();
 
 var app = builder.Build();
 
@@ -12,6 +22,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.MapIdentityApi<AppUser>();
 }
 
 app.UseHttpsRedirection();
